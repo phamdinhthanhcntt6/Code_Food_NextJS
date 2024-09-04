8e70d79b2dbd928264a7edb5d82c20813848c6c7
phamdinhthanhcntt6 <90460582+phamdinhthanhcntt6@users.noreply.github.com>
2024-09-04 16:13:03 +0700

    add http client

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>dadasd111</div>;
+  return (
+    <div>
+      dadasd111
+      <Button>button</Button>
+    </div>
+  );
 }
